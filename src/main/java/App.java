2854e5c02f894675840a42b03773aafc7f1589ff
Krecha <Creeper_G>
2023-10-09 17:47:37 +0300

    branch initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,32 @@
+
+
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.tag.TagException;
+import service.DisplayService;
+import service.FileService;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import service.MenuService;
+
+public class App {
+    public static Logger logger = Logger.getLogger("org.jaudiotagger.audio");
+
+
+    public static void main(String[] args) throws IOException, TagException, InvalidAudioFrameException, ReadOnlyFileException {
+        logger.getParent().setLevel(Level.OFF);
+
+        FileService fileService = new FileService();
+        DisplayService displayService = new DisplayService();
+        MenuService menuService = new MenuService(fileService, displayService);
+
+        String dirPath = displayService.inputDirectory();
+
+        while (!menuService.isNeedStop) {
+             menuService.start(dirPath);
+        }
+    }
+}
